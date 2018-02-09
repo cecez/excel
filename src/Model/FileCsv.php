85163a95a527b7e4a8b368ca3cff89649f4ef8bb
kaleb borda fonseca <kaleb.borda@gmail.com>
2018-02-09 19:54:00 +0000

    initial commit

---
+++ src/Model/FileCsv.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaleb
+ * Date: 2/9/18
+ * Time: 3:47 PM
+ */
+
+namespace fastreading\excel\Model;
+
+
+class FileCsv extends File
+{
+	private $_separadores = [',',';','\t'];
+    protected function readFile()
+    {
+        $this ->_nomeArquivo;
+        $row = 0;
+		if (($handle = fopen($this ->_nomeArquivo, "r")) !== FALSE) {
+
+		    while (($data = fgetcsv($handle, ",")) !== FALSE) {
+		        
+		      	$data=  str_replace($this->_separadores, ',', $data);
+		        $data = explode(',', $data[0]);
+		        $this->file[$row]= $data;
+		        $row++;
+		    }
+		   
+		    fclose($handle);
+		}
+    }
+}
