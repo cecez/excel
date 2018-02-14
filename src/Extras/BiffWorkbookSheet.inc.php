4b011216cb17ac2cfcb291cf093e9d9adca79164
kaleb borda <kalebbf.dev@gmail.com>
2018-02-14 13:29:36 -0200

    ajuste codificação de valores de colunas

--- src/Extras/BiffWorkbookSheet.inc.php
+++ src/Extras/BiffWorkbookSheet.inc.php
@@ -100,16 +100,26 @@ class BiffWorkbookSheet
 	{
 		return $this->_rows;
 	}
-
+	function detectUTF8($string)
+	{
+	    return preg_match('%(?:
+	        [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+	        |\xE0[\xA0-\xBF][\x80-\xBF]        # excluding overlongs
+	        |[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2} # straight 3-byte
+	        |\xED[\x80-\x9F][\x80-\xBF]        # excluding surrogates
+	        |\xF0[\x90-\xBF][\x80-\xBF]{2}     # planes 1-3
+	        |[\xF1-\xF3][\x80-\xBF]{3}         # planes 4-15
+	        |\xF4[\x80-\x8F][\x80-\xBF]{2}     # plane 16
+	        )+%xs', 
+	    $string);
+	}
 	public function addCell ($row, $col, $value, $rawValue, $type, $style)
 	{
 		if ($col >= $this->_cols) $this->_cols = $col + 1;
 		if ($row >= $this->_rows) $this->_rows = $row + 1;
 
-
-        $this->cells [$row][$col] = utf8_encode ($value) ;
-
-		//new BiffWorkbookCell ($this, $row, $col, $value, $rawValue, $type, $style);
+        $this->cells [$row][$col] = ( $this->detectUTF8($value) ?  $value : utf8_encode($value) );
+		
 	}
 }
 ?>
