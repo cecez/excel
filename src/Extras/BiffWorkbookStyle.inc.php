be76c7f75320ca64bfd5c68036a2bfa75e915c55
kaleb borda <kalebbf.dev@gmail.com>
2018-02-19 15:14:17 -0300

    ajuste erro de codificacao

--- src/Extras/BiffWorkbookStyle.inc.php
+++ src/Extras/BiffWorkbookStyle.inc.php
@@ -139,115 +139,115 @@ class BiffWorkbookStyle extends BiffWorkbookElement implements IBiffWorkbookCss
 
 	public function build ()
 	{
-		$this->font = $this->_workbook->fonts [$this->raw ['font']];
-		$this->textAlign = self::$_horAlign [$this->raw ['align'] & 0x07];
-		$this->verticalAlign = self::$_vertAlign [($this->raw ['align'] >> 4) & 0x07];
-		if ($this->_workbook->biffVersion == 5)
-		{
-			$raws = array (
-				'left'		=> array (($this->raw ['border_bg2'] >> 3) & 0x07),
-				'right'		=> array (($this->raw ['border_bg2'] >> 6) & 0x07),
-				'top'		=> array ($this->raw ['border_bg2'] & 0x07),
-				'bottom'	=> array (($this->raw ['border_bg1'] >> 22) & 0x07)
-			);
-			$raws ['left'][1]		= $raws ['left'][0] == 0 ? null : ($this->raw ['border_bg2'] >> 16) & 0x7f;
-			$raws ['right'][1]		= $raws ['right'][0] == 0 ? null : ($this->raw ['border_bg2'] >> 23) & 0x7f;
-			$raws ['top'][1]		= $raws ['top'][0] == 0 ? null : ($this->raw ['border_bg2'] >> 9) & 0x7f;
-			$raws ['bottom'][1]		= $raws ['bottom'][0] == 0 ? null : ($this->raw ['border_bg1'] >> 25) & 0x7f;
+		// $this->font = $this->_workbook->fonts [$this->raw ['font']];
+		// $this->textAlign = self::$_horAlign [$this->raw ['align'] & 0x07];
+		// $this->verticalAlign = self::$_vertAlign [($this->raw ['align'] >> 4) & 0x07];
+		// if ($this->_workbook->biffVersion == 5)
+		// {
+		// 	$raws = array (
+		// 		'left'		=> array (($this->raw ['border_bg2'] >> 3) & 0x07),
+		// 		'right'		=> array (($this->raw ['border_bg2'] >> 6) & 0x07),
+		// 		'top'		=> array ($this->raw ['border_bg2'] & 0x07),
+		// 		'bottom'	=> array (($this->raw ['border_bg1'] >> 22) & 0x07)
+		// 	);
+		// 	$raws ['left'][1]		= $raws ['left'][0] == 0 ? null : ($this->raw ['border_bg2'] >> 16) & 0x7f;
+		// 	$raws ['right'][1]		= $raws ['right'][0] == 0 ? null : ($this->raw ['border_bg2'] >> 23) & 0x7f;
+		// 	$raws ['top'][1]		= $raws ['top'][0] == 0 ? null : ($this->raw ['border_bg2'] >> 9) & 0x7f;
+		// 	$raws ['bottom'][1]		= $raws ['bottom'][0] == 0 ? null : ($this->raw ['border_bg1'] >> 25) & 0x7f;
 
-			foreach ($raws as $type => $value)
-				$this->borders [$type] = new BiffWorkbookBorder (
-					$this->_workbook,
-					array (
-						'type' => $type,
-						'style' => $value [0],
-						'color' => $value [1]
-					)
-				);
+		// 	foreach ($raws as $type => $value)
+		// 		$this->borders [$type] = new BiffWorkbookBorder (
+		// 			$this->_workbook,
+		// 			array (
+		// 				'type' => $type,
+		// 				'style' => $value [0],
+		// 				'color' => $value [1]
+		// 			)
+		// 		);
 
-			$this->pattern = ($this->raw ['border_bg1'] >> 16) & 0x3f;
-			$this->backgroundColor = $this->pattern == 0
-				? $this->_workbook->palette [($this->raw ['border_bg1'] >> 7) & 0x7f]
-				: $this->_workbook->palette [$this->raw ['border_bg1'] & 0x7f];
-		}
-		else
-		{
-			$raws = array (
-				'left' =>		array ($this->raw ['border_bg1'] & 0x0f),
-				'right' =>		array (($this->raw ['border_bg1'] >> 4) & 0x0f),
-				'top' =>		array (($this->raw ['border_bg1'] >> 8) & 0x0f),
-				'bottom' =>		array (($this->raw ['border_bg1'] >> 12) & 0x0f)
-			);
-			$raws ['left'][1]		= $raws ['left'][0] == 0 ? null : ($this->raw ['border_bg1'] >> 16) & 0x7f;
-			$raws ['right'][1]		= $raws ['right'][0] == 0 ? null : ($this->raw ['border_bg1'] >> 23) & 0x7f;
-			$raws ['top'][1]		= $raws ['top'][0] == 0 ? null : $this->raw ['border_bg2'] & 0x7f;
-			$raws ['bottom'][1]		= $raws ['bottom'][0] == 0 ? null : ($this->raw ['border_bg2'] >> 7) & 0x7f;
+		// 	$this->pattern = ($this->raw ['border_bg1'] >> 16) & 0x3f;
+		// 	$this->backgroundColor = $this->pattern == 0
+		// 		? $this->_workbook->palette [($this->raw ['border_bg1'] >> 7) & 0x7f]
+		// 		: $this->_workbook->palette [$this->raw ['border_bg1'] & 0x7f];
+		// }
+		// else
+		// {
+		// 	$raws = array (
+		// 		'left' =>		array ($this->raw ['border_bg1'] & 0x0f),
+		// 		'right' =>		array (($this->raw ['border_bg1'] >> 4) & 0x0f),
+		// 		'top' =>		array (($this->raw ['border_bg1'] >> 8) & 0x0f),
+		// 		'bottom' =>		array (($this->raw ['border_bg1'] >> 12) & 0x0f)
+		// 	);
+		// 	$raws ['left'][1]		= $raws ['left'][0] == 0 ? null : ($this->raw ['border_bg1'] >> 16) & 0x7f;
+		// 	$raws ['right'][1]		= $raws ['right'][0] == 0 ? null : ($this->raw ['border_bg1'] >> 23) & 0x7f;
+		// 	$raws ['top'][1]		= $raws ['top'][0] == 0 ? null : $this->raw ['border_bg2'] & 0x7f;
+		// 	$raws ['bottom'][1]		= $raws ['bottom'][0] == 0 ? null : ($this->raw ['border_bg2'] >> 7) & 0x7f;
 
-			foreach ($raws as $type => $value)
-				$this->borders [$type] = new BiffWorkbookBorder (
-					$this->_workbook,
-					array (
-						'type' => $type,
-						'style' => $value [0],
-						'color' => $value [1]
-					)
-				);
+		// 	foreach ($raws as $type => $value)
+		// 		$this->borders [$type] = new BiffWorkbookBorder (
+		// 			$this->_workbook,
+		// 			array (
+		// 				'type' => $type,
+		// 				'style' => $value [0],
+		// 				'color' => $value [1]
+		// 			)
+		// 		);
 
-			$this->pattern = $this->raw ['border_bg2'] >> 26;
-			$this->backgroundColor = $this->pattern == 0
-				? $this->_workbook->palette [($this->raw ['pattern'] >> 7) & 0x7f]
-				: $this->_workbook->palette [$this->raw ['pattern'] & 0x7f];
-		}
+		// 	$this->pattern = $this->raw ['border_bg2'] >> 26;
+		// 	$this->backgroundColor = $this->pattern == 0
+		// 		? $this->_workbook->palette [($this->raw ['pattern'] >> 7) & 0x7f]
+		// 		: $this->_workbook->palette [$this->raw ['pattern'] & 0x7f];
+		// }
 
-		if (isset (self::$dateFormats [$this->raw ['format']]))
-		{
-			$this->formatType = self::ftDate;
-			$this->format = self::$dateFormats [$this->raw ['format']];
-		}
-		elseif (isset (self::$numberFormats [$this->raw ['format']]))
-		{
-			$this->formatType = self::ftNumber;
-			$this->format = self::$numberFormats [$this->raw ['format']];
-		}
-		elseif (isset ($this->_workbook->formats [$this->raw ['format']]) && $this->_workbook->formats [$this->raw ['format']] != 'GENERAL')
-		{
-			if (preg_match ('/[^hmsday\/\-:\s]/i', $this->_workbook->formats [$this->raw ['format']]) == 0)
-			{
-				$this->formatType = self::ftDate;
-				$this->format = str_replace (
-					array ('mm', 'h', 'DD', 'MM', 'YY', 'YYYY'),
-					array ('i', 'H', 'd', 'm', 'y', 'Y'),
-					$this->_workbook->formats [$this->raw ['format']]
-				);
-			}
-			else
-			{
-				$this->formatType = self::ftGeneral;
-				$this->format = $this->_workbook->formats [$this->raw ['format']];
-			}
-		}
-		else
-		{
-			$this->formatType = self::ftGeneral;
-			$this->format = null;
-		}
+		// if (isset (self::$dateFormats [$this->raw ['format']]))
+		// {
+		// 	$this->formatType = self::ftDate;
+		// 	$this->format = self::$dateFormats [$this->raw ['format']];
+		// }
+		// elseif (isset (self::$numberFormats [$this->raw ['format']]))
+		// {
+		// 	$this->formatType = self::ftNumber;
+		// 	$this->format = self::$numberFormats [$this->raw ['format']];
+		// }
+		// elseif (isset ($this->_workbook->formats [$this->raw ['format']]) && $this->_workbook->formats [$this->raw ['format']] != 'GENERAL')
+		// {
+		// 	if (preg_match ('/[^hmsday\/\-:\s]/i', $this->_workbook->formats [$this->raw ['format']]) == 0)
+		// 	{
+		// 		$this->formatType = self::ftDate;
+		// 		$this->format = str_replace (
+		// 			array ('mm', 'h', 'DD', 'MM', 'YY', 'YYYY'),
+		// 			array ('i', 'H', 'd', 'm', 'y', 'Y'),
+		// 			$this->_workbook->formats [$this->raw ['format']]
+		// 		);
+		// 	}
+		// 	else
+		// 	{
+		// 		$this->formatType = self::ftGeneral;
+		// 		$this->format = $this->_workbook->formats [$this->raw ['format']];
+		// 	}
+		// }
+		// else
+		// {
+		// 	$this->formatType = self::ftGeneral;
+		// 	$this->format = null;
+		// }
 	}
 
 	public function css ()
 	{
-		$result = $this->font->css ();
-		foreach ($this->borders as $border)
-			$result .= ' ' . $border->css ();
-		$result .= ' ' . implode (
-			'; ',
-			array (
-				'text-align: ' . $this->textAlign,
-				'vertical-align: ' . $this->verticalAlign,
-				'background-color: ' . $this->backgroundColor
-			)
-		) . ';';
+		// $result = $this->font->css ();
+		// foreach ($this->borders as $border)
+		// 	$result .= ' ' . $border->css ();
+		// $result .= ' ' . implode (
+		// 	'; ',
+		// 	array (
+		// 		'text-align: ' . $this->textAlign,
+		// 		'vertical-align: ' . $this->verticalAlign,
+		// 		'background-color: ' . $this->backgroundColor
+		// 	)
+		// ) . ';';
 
-		return $result;
+		return "";
 	}
 }
 ?>
