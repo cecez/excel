1542744860c99e9fe0ec597cb54d65e44521923b
kaleb borda <kalebbf.dev@gmail.com>
2018-03-01 16:40:57 -0300

    ajuste csv delimiters

--- src/Model/FileCsv.php
+++ src/Model/FileCsv.php
@@ -11,20 +11,24 @@ namespace fastreading\excel\Model;
 
 class FileCsv extends File
 {
-	private $_separadores = [',',';','\t'];
+	private $_separadores = [',',';',"  "];
     protected function readFile()
     {
-        $this ->_nomeArquivo;
-        $row = 0;
+         $row = 0;
+//        dd($this->getFileDelimiter($this ->_nomeArquivo));
 		if (($handle = fopen($this ->_nomeArquivo, "r")) !== FALSE) {
+//        dd($handle );
+		    while (($data = fgetcsv($handle,',')) !== FALSE) {
 
-		    while (($data = fgetcsv($handle, ",")) !== FALSE) {
-		        
 		      	$data=  str_replace($this->_separadores, ',', $data);
-		        $this->file[$row]= $data;
+
+		      	if(isset($data[0]) and !is_array($data[0]) and count($data) <= 1   ){
+
+		      	    $data = explode(',',$data[0]);
+                }
+		        $this->file[$row]= str_replace('"','',$data);
 		        $row++;
 		    }
-		   
 		    fclose($handle);
 		}
     }
